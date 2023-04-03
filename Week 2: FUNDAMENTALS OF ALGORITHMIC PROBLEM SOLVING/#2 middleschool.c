c47d63b3aa63e72ab1d50c6f57ed913763d80d11
Kumud <88158584+knowkumud@users.noreply.github.com>
2023-04-03 06:26:07 +0530

    Update #2 middleschool.c

--- Week 2: FUNDAMENTALS OF ALGORITHMIC PROBLEM SOLVING/#2 middleschool.c
+++ Week 2: FUNDAMENTALS OF ALGORITHMIC PROBLEM SOLVING/#2 middleschool.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-void main()
+int gcd_middle(int m , int n)
 {
-int m,n,t,count=0,i;
-//printf("Enter the First Number : \n");
-//printf("Enter the Second Number : \n");
-for(i = 0 ; i < 5 ; i++)
-{
-scanf("%d", &m);
-scanf("%d", &n);
-for(t = (m <= n) ? m : n ; t>0 ; t--)
+if(m > n)
+gcd_middle(n , m);
+int fact = 2;
+int gcd = 1;
+int k = m;
+int count = 0;
+for(fact = 2; fact <= k; fact++)
 {
 count++;
-if(m%t == 0 && n%t == 0)
+while(((m % fact) == 0) && ((n % fact) == 0))
 {
-//printf("The GCD of '%d' and ' %d' = '%d'\n",m,n,t);
-printf("%d	%d\n",t,count);
-break;
+count++;
+gcd = gcd * fact;
+n = n / fact;
+m = m / fact;
+if (m == 0 || n == 0)
+{
+printf("%d\t",count);
+return gcd;
 }
 }
-//printf("Count = '%d'\n",count);
+}
+printf("%d\t",count);
+return gcd;
+}
+void main()
+{
+//printf("Enter the First Number : \n");
+//printf("Enter the Second Number : \n");
+//printf("The GCD of '%d' and '%d' = '%d' \n",m,n,gcd_middle(m,n));
+int i;
+for(i = 0 ; i < 5 ; i++)
+{
+int m , n;
+scanf("%d", &m);
+scanf("%d", &n);
+printf("%d\n",gcd_middle(m,n));
 }
 }
